87b53aa128b1ff6761eff33f4391443e1b35eee5
phil cowan <philipjcowan1@gmail.com>
2020-04-21 10:06:28 -0700

    setting up mongoose db and bookschema

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -18,6 +20,9 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+//connect to the Mongo Db
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
