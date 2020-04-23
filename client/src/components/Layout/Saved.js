19d34dc1666fa91e88e6eb5a08dbdbc7f36d4c66
phil cowan <philipjcowan1@gmail.com>
2020-04-23 16:45:39 -0700

    trying to fix deployment

--- client/src/components/Layout/Saved.js
+++ client/src/components/Layout/Saved.js
@@ -29,20 +29,34 @@ export class Saved extends Component {
     render() {
         return (
             <React.Fragment>
-                <ul>
-                    {this.state.savedBooks.map(result => (
-                        <li className="list-group-item" key={result._id}>
-                            <h3>{result.title}</h3>
-                            <h3>-{result.author}</h3>
-                            <img alt={result.title} className="img-fluid" src={result.image} />
-                            <p>Description: {result.synopsis}</p>
-                            <a target="_blank" href={result.link}>infoLink</a>
-                            <button result={result} onClick={() => this.delBook(result._id)} dataid={result._id}>delete book</button>
-                        </li>))}
-                </ul>
+                <div style={resultsContainer}>
+                    <ul>
+                        {this.state.savedBooks.map(result => (
+                            <li className="list-group-item" style={listStyle} key={result._id}>
+                                <h3>{result.title}</h3>
+                                <h3>-{result.author}</h3>
+                                <img alt={result.title} className="img-fluid" src={result.image} />
+                                <p>Description: {result.synopsis}</p>
+                                <a target="_blank" href={result.link}>infoLink</a>
+                                <button result={result} onClick={() => this.delBook(result._id)} dataid={result._id}>delete book</button>
+                            </li>))}
+                    </ul>
+                </div>
             </React.Fragment>
         )
     }
 }
 
 export default Saved
+
+const listStyle = {
+    background: 'grey',
+    listStyle: 'none',
+    maxWidth: '90%'
+}
+const resultsContainer = {
+    margin: '5%',
+    maxWidth: '50%',
+    border: '2px solid black',
+    padding: '15px'
+}
